9e76addd310f779409b7afa6240ed46a6003a0c7
MUYU_Twilighter <1484605372@qq.com>
2024-12-31 03:38:33 -0500

    fix icon of creative mode tab optimize build artifact name

--- common/src/main/java/cool/muyucloud/registry/Tabs.java
+++ common/src/main/java/cool/muyucloud/registry/Tabs.java
@@ -9,7 +9,6 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Items;
 
 import java.util.function.Supplier;
 
