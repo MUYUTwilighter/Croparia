cb38829dddf038fb4697a1cfc0b5a6d6e4e08c73
MUYU_Twilighter <1484605372@qq.com>
2025-01-20 21:52:38 -0500

    - Fix forge event bus - Add RER for runtime

--- forge/src/main/java/cool/muyucloud/croparia/forge/CropariaIfForge.java
+++ forge/src/main/java/cool/muyucloud/croparia/forge/CropariaIfForge.java
@@ -2,14 +2,14 @@ package cool.muyucloud.croparia.forge;
 
 import cool.muyucloud.croparia.CropariaIf;
 import dev.architectury.platform.forge.EventBuses;
-import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.event.server.ServerStoppingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(CropariaIf.MOD_ID)
-@Mod.EventBusSubscriber(modid = CropariaIf.MOD_ID, value = Dist.DEDICATED_SERVER)
+@Mod.EventBusSubscriber(modid = CropariaIf.MOD_ID)
 public class CropariaIfForge {
     public CropariaIfForge() {
         EventBuses.registerModEventBus(CropariaIf.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
@@ -18,7 +18,7 @@ public class CropariaIfForge {
     }
 
     @SubscribeEvent
-    public static void onServerStarting() {
+    public static void onServerStarting(ServerStartingEvent event) {
         CropariaIf.onServerStarting();
     }
 
