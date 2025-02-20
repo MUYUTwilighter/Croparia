9a41696b8598efc300879bba537df2a83e040887
MUYU_Twilighter <1484605372@qq.com>
2025-02-20 04:49:06 -0500

    optimize mixin

--- common/src/main/java/cool/muyucloud/croparia/mixin/DropperBlockMixin.java
+++ common/src/main/java/cool/muyucloud/croparia/mixin/DropperBlockMixin.java
@@ -1,4 +1,4 @@
-package cool.muyucloud.croparia.fabric.mixin;
+package cool.muyucloud.croparia.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import cool.muyucloud.croparia.util.ItemPlaceable;
