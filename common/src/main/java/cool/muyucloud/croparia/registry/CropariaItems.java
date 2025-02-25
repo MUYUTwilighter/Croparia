6b5128b6f6dff8f6fadabb37275f975a027440aa
MUYU_Twilighter <1484605372@qq.com>
2025-02-25 18:03:22 -0500

    Add command `/croparia create` to create crop definition

--- common/src/main/java/cool/muyucloud/croparia/registry/CropariaItems.java
+++ common/src/main/java/cool/muyucloud/croparia/registry/CropariaItems.java
@@ -12,6 +12,7 @@ import cool.muyucloud.croparia.api.core.item.relic.MidasHand;
 import cool.muyucloud.croparia.api.crop.Crop;
 import cool.muyucloud.croparia.api.crop.item.CropFruit;
 import cool.muyucloud.croparia.api.crop.item.CropSeed;
+import cool.muyucloud.croparia.api.crop.item.Croparia;
 import cool.muyucloud.croparia.api.element.ElementsEnum;
 import cool.muyucloud.croparia.api.element.item.Elematilius;
 import cool.muyucloud.croparia.api.element.item.ElementalBucket;
@@ -114,20 +115,34 @@ public class CropariaItems {
         "elemental_air",
         () -> new Elematilius(ElementsEnum.AIR, new Item.Properties().arch$tab(Tabs.MAIN))
     );
+    @PostReg
     public static final RegistrySupplier<Item> CROPARIA = registerItem(
-        "croparia", () -> new Item(new Item.Properties().arch$tab(Tabs.MAIN)));
+        "croparia", () -> new Croparia(1, new Item.Properties().arch$tab(Tabs.MAIN))
+    );
+    @PostReg
     public static final RegistrySupplier<Item> CROPARIA2 = registerItem(
-        "croparia2", () -> new Item(new Item.Properties().arch$tab(Tabs.MAIN)));
+        "croparia2", () -> new Croparia(2, new Item.Properties().arch$tab(Tabs.MAIN))
+    );
+    @PostReg
     public static final RegistrySupplier<Item> CROPARIA3 = registerItem(
-        "croparia3", () -> new Item(new Item.Properties().arch$tab(Tabs.MAIN)));
+        "croparia3", () -> new Croparia(3, new Item.Properties().arch$tab(Tabs.MAIN))
+    );
+    @PostReg
     public static final RegistrySupplier<Item> CROPARIA4 = registerItem(
-        "croparia4", () -> new Item(new Item.Properties().arch$tab(Tabs.MAIN)));
+        "croparia4", () -> new Croparia(4, new Item.Properties().arch$tab(Tabs.MAIN))
+    );
+    @PostReg
     public static final RegistrySupplier<Item> CROPARIA5 = registerItem(
-        "croparia5", () -> new Item(new Item.Properties().arch$tab(Tabs.MAIN)));
+        "croparia5", () -> new Croparia(5, new Item.Properties().arch$tab(Tabs.MAIN))
+    );
+    @PostReg
     public static final RegistrySupplier<Item> CROPARIA6 = registerItem(
-        "croparia6", () -> new Item(new Item.Properties().arch$tab(Tabs.MAIN)));
+        "croparia6", () -> new Croparia(6, new Item.Properties().arch$tab(Tabs.MAIN))
+    );
+    @PostReg
     public static final RegistrySupplier<Item> CROPARIA7 = registerItem(
-        "croparia7", () -> new Item(new Item.Properties().arch$tab(Tabs.MAIN)));
+        "croparia7", () -> new Croparia(7, new Item.Properties().arch$tab(Tabs.MAIN))
+    );
     public static final RegistrySupplier<HornPlenty> HORN = registerItem("horn_plenty", HornPlenty::new);
     public static final RegistrySupplier<InfiniteApple> INFINITE_APPLE = registerItem("infinite_apple", InfiniteApple::new);
     public static final RegistrySupplier<MagicRope> MAGIC_ROPE = registerItem("magic_rope", MagicRope::new);
