fa17219f8f53d543e8b3c09bc8d1291cb3959122
MUYU_Twilighter <1484605372@qq.com>
2025-04-14 22:35:29 -0400

    sync changes from 1.20.1

---
+++ common/src/main/java/cool/muyucloud/croparia/mixin/ReloadCommandMixin.java
@@ -0,0 +1,21 @@
+package cool.muyucloud.croparia.mixin;
+
+import cool.muyucloud.croparia.CropariaIf;
+import cool.muyucloud.croparia.api.generator.pack.DataPackHandler;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.server.commands.ReloadCommand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Collection;
+
+@Mixin(ReloadCommand.class)
+public class ReloadCommandMixin {
+    @Inject(method = "reloadPacks", at = @At("HEAD"))
+    private static void onReload(Collection<String> collection, CommandSourceStack commandSourceStack, CallbackInfo ci) {
+        DataPackHandler.INSTANCE.beforeReload();
+        CropariaIf.LOGGER.info("Data pack generation performed");
+    }
+}
