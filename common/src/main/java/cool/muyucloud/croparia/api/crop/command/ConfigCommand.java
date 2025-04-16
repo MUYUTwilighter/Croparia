c787e3f6b1e5765dde3aa5056e064c4e02ede5e8
MUYU_Twilighter <1484605372@qq.com>
2025-04-16 19:10:31 -0400

    sync changes from 1.21.3

--- common/src/main/java/cool/muyucloud/croparia/api/crop/command/ConfigCommand.java
+++ common/src/main/java/cool/muyucloud/croparia/api/crop/command/ConfigCommand.java
@@ -25,6 +25,30 @@ public class ConfigCommand {
         context.getSource().sendSuccess(() -> Component.translatable("commands.croparia.config.ritual", CropariaIf.CONFIG.getRitual().toString()), false);
         return 1;
     }));
+    private static final LiteralArgumentBuilder<CommandSourceStack> FRUIT_USE = Commands.literal("fruitUse").executes(context -> {
+        context.getSource().sendSuccess(() -> Component.translatable("commands.croparia.config.fruitUse", CropariaIf.CONFIG.getFruitUse().toString()), false);
+        return 1;
+    }).then(Commands.argument("value", BoolArgumentType.bool()).executes(context -> {
+        CropariaIf.CONFIG.setFruitUse(BoolArgumentType.getBool(context, "value"));
+        context.getSource().sendSuccess(() -> Component.translatable("commands.croparia.config.fruitUse", CropariaIf.CONFIG.getFruitUse().toString()), false);
+        return 1;
+    }));
+    private static final LiteralArgumentBuilder<CommandSourceStack> AUTO_RELOAD = Commands.literal("dump").executes(context -> {
+        context.getSource().sendSuccess(() -> Component.translatable("commands.croparia.config.autoReload", CropariaIf.CONFIG.getAutoReload().toString()), false);
+        return 1;
+    }).then(Commands.argument("value", BoolArgumentType.bool()).executes(context -> {
+        CropariaIf.CONFIG.setAutoReload(BoolArgumentType.getBool(context, "value"));
+        context.getSource().sendSuccess(() -> Component.translatable("commands.croparia.config.autoReload", CropariaIf.CONFIG.getAutoReload().toString()), false);
+        return 1;
+    }));
+    private static final LiteralArgumentBuilder<CommandSourceStack> OVERRIDE = Commands.literal("OVERRIDE").executes(context -> {
+        context.getSource().sendSuccess(() -> Component.translatable("commands.croparia.config.override", CropariaIf.CONFIG.getOverride().toString()), false);
+        return 1;
+    }).then(Commands.argument("value", BoolArgumentType.bool()).executes(context -> {
+        CropariaIf.CONFIG.setOverride(BoolArgumentType.getBool(context, "value"));
+        context.getSource().sendSuccess(() -> Component.translatable("commands.croparia.config.override", CropariaIf.CONFIG.getOverride().toString()), false);
+        return 1;
+    }));
 
     public static ArgumentBuilder<CommandSourceStack, ?> buildInfusor() {
         return INFUSOR;
@@ -32,5 +56,17 @@ public class ConfigCommand {
 
     public static ArgumentBuilder<CommandSourceStack, ?> buildRitual() {
         return RITUAL;
+    }
+
+    public static ArgumentBuilder<CommandSourceStack, ?> buildFruitUse() {
+        return FRUIT_USE;
+    }
+
+    public static ArgumentBuilder<CommandSourceStack, ?> buildAutoReload() {
+        return AUTO_RELOAD;
+    }
+
+    public static ArgumentBuilder<CommandSourceStack, ?> buildOverride() {
+        return OVERRIDE;
     }
 }
