ab6f3877d6dd8ad2e9ca628f80b702bc242c48b7
MUYU_Twilighter <1484605372@qq.com>
2025-04-17 01:45:12 -0400

    fix bugs

--- common/src/main/java/cool/muyucloud/croparia/api/generator/pack/ResourcePackHandler.java
+++ common/src/main/java/cool/muyucloud/croparia/api/generator/pack/ResourcePackHandler.java
@@ -29,17 +29,10 @@ public class ResourcePackHandler extends PackHandler {
     );
 
     @Override
-    public void onInitial() {
-        super.onInitial();
-        if (CropariaIf.CONFIG.getOverride()) {
-            this.clear();
-        }
+    public void beforeReload() {
+        super.beforeReload();
         CropariaIf.LOGGER.info("Generating resource pack data to file system");
         this.dump();
-    }
-
-    @Override
-    public void onSecondary() {
     }
 
     @Override
