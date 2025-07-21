431766d565d36b5514d4a0fba5ce9c504c3c016e
MUYU_Twilighter <1484605372@qq.com>
2025-07-21 18:27:42 +0800

    fix forgotten changes

--- fabric/src/main/java/cool/muyucloud/croparia/fabric/CropariaIfFabric.java
+++ fabric/src/main/java/cool/muyucloud/croparia/fabric/CropariaIfFabric.java
@@ -2,15 +2,11 @@ package cool.muyucloud.croparia.fabric;
 
 import cool.muyucloud.croparia.CropariaIf;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
 public class CropariaIfFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         CompatCrops.init();
         CropariaIf.init();
-        ServerLifecycleEvents.SERVER_STARTING.register(server -> CropariaIf.onServerStarting());
-        ServerLifecycleEvents.SERVER_STARTED.register(CropariaIf::onServerStarted);
-        ServerLifecycleEvents.SERVER_STOPPING.register(server -> CropariaIf.onServerStopping());
     }
 }
